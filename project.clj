8c540786bc04ae6953cef0fdb466c416bc3ef168
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2014-03-12 17:24:53 +0600

    update dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject skeletom "0.1.0-SNAPSHOT"
   :description "Boilerplate ClojureScript application with Om"
   :url "https://github.com/StepanKuzmin/skeletom"
-  :dependencies [[org.clojure/clojurescript "0.0-2156"]
-                 [om "0.5.0-rc1"]
-                 [com.facebook/react "0.9.0-rc1"]]
+  :dependencies [[org.clojure/clojurescript "0.0-2173"]
+                 [om "0.5.2"]
+                 [com.facebook/react "0.9.0"]]
   :plugins [[lein-cljsbuild "1.0.2"]]
   :source-paths ["src"]
   :cljsbuild {
