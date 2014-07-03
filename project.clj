01ed440ec129ee2fd14f86869229cedbe638a657
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2014-07-03 16:39:18 +0600

    update dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject skeletom "0.1.0-SNAPSHOT"
   :description "Boilerplate ClojureScript application with Om"
   :url "https://github.com/StepanKuzmin/skeletom"
-  :dependencies [[org.clojure/clojurescript "0.0-2173"]
-                 [om "0.5.2"]
-                 [com.facebook/react "0.9.0"]]
-  :plugins [[lein-cljsbuild "1.0.2"]]
+  :dependencies [[org.clojure/clojurescript "0.0-2227"]
+                 [com.facebook/react "0.9.0.4"]
+                 [ankha "0.1.3"]
+                 [om "0.6.4"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
   :source-paths ["src"]
   :cljsbuild {
     :builds [{
--- src/skeletom/app.cljs
+++ src/skeletom/app.cljs
@@ -1,5 +1,15 @@
 (ns skeletom.app
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [ankha.core :as ankha]))
 
 (enable-console-print!)
+
+(om/root
+ ankha/inspector
+ {:points [{:x 1 :y 0}
+           {:x 0 :y 1}
+           {:x -1 :y 0}
+           {:x 0 :y -1}]}
+
+ {:target (js/document.getElementById "app")})
